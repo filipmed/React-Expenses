9a94ae73abedc82dbc7e76de33ed298e346e1515
Filiberto Medina <darkghost595@gmail.com>
2022-10-10 17:55:18 -0700

    2nd push

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,10 @@
 import React,{ useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
+
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from "./ExpensesFilter";
-
+import ExpensesList from "./ExpensesList";
 const Expenses=(props)=>{
        
     const [filteredYear,setFilteredYear]= useState('2020');
@@ -18,21 +18,15 @@ const Expenses=(props)=>{
     return expense.date.getFullYear().toString()===filteredYear;
   })
     
+  
+
   return(
         <div>
 
         <Card className="Expenses">
         <ExpensesFilter selected = {filteredYear} onChangeFilter={filterChangeHandler} 
         />
-         
-          {filteredExpense.map((expense) => (
-          <ExpenseItem 
-          key={expense.id}
-          title={expense.title} 
-          amount={expense.amount} 
-          date={expense.date}
-          />
-          ))}
+         <ExpensesList items={filteredExpense}/>
         </Card>
         </div>
     )
